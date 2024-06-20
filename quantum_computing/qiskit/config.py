ffa5d0a0e97a9d5ea4572311d5617764f660421f
KOSASIH <kosasihg88@gmail.com>
2024-06-20 12:30:11 +0700

    Create config.py

---
+++ quantum_computing/qiskit/config.py
@@ -0,0 +1,19 @@
+# config.py
+import os
+
+# Qiskit configuration
+QISKIT_API_TOKEN = 'YOUR_API_TOKEN'
+QISKIT_API_URL = 'https://api.quantum-computing.ibm.com/api'
+
+# IBM Quantum configuration
+IBM_QUANTUM_API_TOKEN = 'YOUR_API_TOKEN'
+IBM_QUANTUM_API_URL = 'https://quantum-computing.ibm.com/api'
+
+# Quantum circuit configuration
+QUANTUM_CIRCUIT_NAME = 'globalmind_quantum_circuit'
+QUANTUM_CIRCUIT_QUBITS = 5
+QUANTUM_CIRCUIT_DEPTH = 3
+
+# Logging configuration
+LOGGING_LEVEL = 'DEBUG'
+LOGGING_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
