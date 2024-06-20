2c8e861cb5027fa26c7126fdc2fc8c4fcd66ef84
KOSASIH <kosasihg88@gmail.com>
2024-06-20 12:31:42 +0700

    Create config.js

---
+++ quantum_computing/ibm-quantum/config.js
@@ -0,0 +1,11 @@
+// config.js
+export default {
+  // IBM Quantum API configuration
+  apiToken: 'YOUR_API_TOKEN',
+  apiUrl: 'https://quantum-computing.ibm.com/api',
+
+  // Quantum circuit configuration
+  circuitName: 'globalmind_quantum_circuit',
+  circuitQubits: 5,
+  circuitDepth: 3
+};
